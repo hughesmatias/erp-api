d333d0d719c58acacf16d8e80244e85c013ecc96
Matias Perez Hughes <hughesmatias@gmail.com>
2020-06-30 20:57:23 -0300

    Database - Integrate sequelize

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const models = require('./models');
 
 const port = process.env.PORT;
 
@@ -8,4 +9,6 @@ app.get('/version', (req, res) => {
   res.end('version 1.0');
 });
 
-app.listen(5000, () => console.log(`Running in 5000`));
+models.sequelize.sync().then(function() {
+  app.listen(5000, () => console.log(`Running in 5000`));
+});
