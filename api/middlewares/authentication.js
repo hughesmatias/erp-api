51e4fde2f883417436c93e2a0890b219c7aaba0c
Matias Perez Hughes <hughesmatias@gmail.com>
2020-07-05 16:19:13 -0300

    token validation.

--- api/middlewares/authentication.js
+++ api/middlewares/authentication.js
@@ -1,12 +1,24 @@
 const utils = require('../../utils/auth');
-module.exports = (req, res, next) => {
-  const token = req.header['Authorization'] && req.header['Authorization'].split(" ");
+const models = require('../../models');
+
+module.exports = async (req, res, next) => {
+  const token = req.headers['authorization'] && req.headers['authorization'].split(" ");
   if (token && token[0] == 'Bearer') {
     try {
-      utils.validateToken(token) && next();
-    } catch(err) {
-      res.status(401).send(err);
+      const session = await utils.validateToken(token[1]);
+      const userExist = await models.User.find({ where: { email: session.email }});
+      if (userExist) {
+        req.userData = session;
+        next();
+      } else {
+        throw new Error("Valide el usuario logeado.");
+      }
+    } catch({ message }) {
+      res.status(401).json({
+        message
+      }).end();
     }
+  } else {
+    res.status(403).end();
   }
-  res.status(403).end();
 };
