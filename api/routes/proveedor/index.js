365f3300fed46a0c155a1a7d7954f225d5098cb3
Matias Perez Hughes <hughesmatias@gmail.com>
2020-07-10 20:15:03 -0300

    crud proveedor

--- api/routes/proveedor/index.js
+++ api/routes/proveedor/index.js
@@ -2,21 +2,32 @@ const express = require('express');
 let router  = express.Router();
 
 module.exports = models => {
-  router.get('/add', (req, res) => {
-    models.User.create({
-      nombre: 'john-doe',
-      email: 'asd@s.co',
-    }).then(function(user) {
-      res.json(user);
-    })
-  })
-  
-  router.get('/', function(req, res) {
-    models.User.findAll().then(function(users) {
-      res.json(
-        users
-      );
-    });
+  router.post('/', async (req, res) => {
+    const newProveedor = await models.Proveedor.create(req.body)
+    res.send(newProveedor);
+  });
+
+  router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const proveedor = await models.Proveedor.update({ ...req.body }, { where: { id } });
+    res.send(proveedor);
+  });
+
+  router.get('/', async (req, res) => {
+    const proveedores = await models.Proveedor.findAll();
+    res.send(proveedores);
+  });
+
+  router.get('/:id', async (req, res) => {
+    const { id  } = req.params;
+    const proveedor = await models.Proveedor.findOne({ where: { id }});
+    res.send(proveedor);
+  });
+
+  router.delete('/:id', async (req, res) => {
+    const { id  } = req.params;
+    const deleted = await models.Proveedor.destroy(id);
+    res.send(deleted);
   });
 
   return router;
